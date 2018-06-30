d4b3eebf85cdba5039747ba3acb812561e1d8669
twinie96 <twinie96@gmail.com>
2018-06-30 12:36:24 +0200

    add comments to ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,6 @@
 class Idea < ApplicationRecord
+  has_many :comments
+
   validates :name, presence: true
 
   has_one_attached :avatar
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_30_081655) do
+ActiveRecord::Schema.define(version: 2018_06_30_095200) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2018_06_30_081655) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ideas", force: :cascade do |t|
